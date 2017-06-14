f7c4e6f362ec7f596ffff57004d4934026c50a75
Yoann Dubreuil <yoann.dubreuil@gmail.com>
2017-06-14 14:49:39 +0200

    fix official Docker image build
    install-plugins.sh bundles incompatible plugins with Jenkins LTS, so we
    rely on Maven to download plugins instead

--- docker/official/build.sh
+++ docker/official/build.sh
@@ -3,7 +3,9 @@
 HERE=$(dirname $0)
 cd $HERE
 
-BLUEOCEAN_VERSION=$(git for-each-ref --sort=-taggerdate refs/tags/blueocean-parent-\*  | grep -v 'beta' | head -1 |awk '{print $3 }' | sed 's/refs\/tags\/blueocean-parent-//')
+# Useful to rebuild an already existing image, in case it's broken
+FORCE_REBUILD=${FORCE_REBUILD:-$false}
+BLUEOCEAN_VERSION=$(git for-each-ref --sort=-taggerdate refs/tags/blueocean-parent-\*  | head -1 |awk '{print $3 }' | sed 's/refs\/tags\/blueocean-parent-//')
 
 # Check we have the latest LTS for blue ocean to build on and get its image ID
 docker pull jenkinsci/jenkins:lts-alpine
@@ -14,11 +16,20 @@ LTS_IMAGE_ID=$(docker images jenkinsci/jenkins:lts-alpine | sed -n 2p | awk '{ p
 # A new LTS means there may be security fixes
 FULL_VERSION="$BLUEOCEAN_VERSION-$LTS_IMAGE_ID"
 
-# Check if the image already exists
-if docker pull jenkinsci/blueocean:$FULL_VERSION; then
-  echo "Image jenkinsci/blueocean:$FULL_VERSION already exists in Docker Hub"
-  exit 0
+if [ ! $FORCE_REBUILD ]; then
+  # Check if the image already exists
+  if docker pull jenkinsci/blueocean:$FULL_VERSION; then
+    echo "Image jenkinsci/blueocean:$FULL_VERSION already exists in Docker Hub"
+    exit 0
+  fi
 fi
+
+# Fetch BlueOcean plugins using Maven
+#
+# Note: we don't use "install-plugins.sh" script from the official Jenkins Docker image because we want to use Maven to resolve
+# plugin dependency. For BO we will all blueocean-* plugins to be the same version. The aforementioned script downloads latest version
+# of dependent plugins, so we would need to list all BO plugins and do it in the right order (if there's any)
+docker run -i --rm -v "$PWD":/usr/src/boplugins -w /usr/src/boplugins -u "$(id -u)" maven:3.3.9 mvn "-Dblueocean.version=$BLUEOCEAN_VERSION" package
 
 # Build the image
 docker build --rm --no-cache --pull \
